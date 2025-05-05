a108731ef401fca8feb0cc7aac21feb1e3e729e3
Alinebcc2467 <alinebcc2467@gmail.com>
2025-05-05 09:16:06 -0300

    Add files via upload

---
+++ Atividades em java/E.S.7.java
@@ -0,0 +1,13 @@
+//7 - Faça um programa que leia uma temperatura em graus Celsius e apresente-a convertida em graus Fahrenheit. A fórmula de conversão é: ℉ = (9 × ℃ + 160) ÷ 5, na qual ℉ é a temperatura em Fahrenheit e ℃ é a temperatura em Celsius.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Escreva o valor para calcular: ");
+        float valor = s.nextFloat();
+        float f = ((valor * 9) + 160)/5;
+        System.out.println("A temperatura " + valor + "°C em Fahrenheit é " + f + "°F");
+    }
+}
