51784a352bf79eda220084d9d5e999ba6172f13e
Alinebcc2467 <alinebcc2467@gmail.com>
2025-05-13 08:56:42 -0300

    Update and rename E.D.9.java to EstruturaDecisão.9.java

--- Atividades em java/EstruturaDecisão.9.java
+++ Atividades em java/EstruturaDecisão.9.java
@@ -1,4 +1,7 @@
-//9 - Faça um programa que efetue a leitura de três valores numéricos representando os lados de um triângulo. O programa deverá verificar e informar se os lados fornecidos formam realmente um triângulo (cada lado é menor que a soma dos outros dois lados). Se esta condição for verdadeira, deverá ser indicado qual tipo de triângulo foi formado: isósceles (dois lados iguais e um diferente), escaleno (todos os lados diferentes) ou eqüilátero (todos os lados são iguais).
+//9 - Faça um programa que efetue a leitura de três valores numéricos representando os lados de um triângulo. O programa deverá 
+//verificar e informar se os lados fornecidos formam realmente um triângulo (cada lado é menor que a soma dos outros dois lados). 
+//Se esta condição for verdadeira, deverá ser indicado qual tipo de triângulo foi formado: isósceles (dois lados iguais e um 
+//diferente), escaleno (todos os lados diferentes) ou eqüilátero (todos os lados são iguais).
 
 import java.util.Scanner;
 
