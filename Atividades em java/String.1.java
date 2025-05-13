6e4aac5537fb204c6967724b9b8b339a3abb6bad
Alinebcc2467 <alinebcc2467@gmail.com>
2025-05-13 09:03:26 -0300

    Update and rename S.1.java to String.1.java

--- Atividades em java/String.1.java
+++ Atividades em java/String.1.java
@@ -1,4 +1,5 @@
-//1 - Faça um programa que permita ao usuário digitar o seu nome e em seguida mostre o nome do usuário de trás para frente utilizando somente letras maiúsculas. Dica: lembre−se que ao informar o nome o usuário pode digitar letras maiúsculas ou minúsculas.
+//1 - Faça um programa que permita ao usuário digitar o seu nome e em seguida mostre o nome do usuário de trás para frente utilizando 
+//somente letras maiúsculas. Dica: lembre−se que ao informar o nome o usuário pode digitar letras maiúsculas ou minúsculas.
 
 import java.util.Scanner;
 
