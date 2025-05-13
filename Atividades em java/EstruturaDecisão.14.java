d81cce7a7623f995ec6827304bcf20df659ed934
Alinebcc2467 <alinebcc2467@gmail.com>
2025-05-13 08:55:13 -0300

    Update and rename E.D.14.java to EstruturaDecisão.14.java

--- Atividades em java/EstruturaDecisão.14.java
+++ Atividades em java/EstruturaDecisão.14.java
@@ -1,4 +1,6 @@
-//14 - Uma quitanda está vendendo frutas com a seguinte tabela de preços. Se o cliente comprar mais de 8 Kg em frutas ou o valor total da compra ultrapassar R$ 25,00, receberá ainda um desconto de 10% sobre este total. Escreva um algoritmo para ler a quantidade (em Kg) de morangos e a quantidade (em Kg) de maças adquiridas e escreva o valor a ser pago pelo cliente.
+//14 - Uma quitanda está vendendo frutas com a seguinte tabela de preços. Se o cliente comprar mais de 8 Kg em frutas ou o valor 
+//total da compra ultrapassar R$ 25,00, receberá ainda um desconto de 10% sobre este total. Escreva um algoritmo para ler a 
+//quantidade (em Kg) de morangos e a quantidade (em Kg) de maças adquiridas e escreva o valor a ser pago pelo cliente.
 //
 //           Até 5Kg	  Acima de 5kg
 //Morango	R$2,50 / kg	  R$2,20 / kg
